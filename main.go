7007e7be3322930d2b175d88e543e43428a9fd51
Kyler Wong <wsm.kyler@gmail.com>
2021-01-29 17:47:15 +0800

    feat: add dotenv for local dev

--- main.go
+++ main.go
@@ -3,10 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/kylerwsm/bounce/pkg/services"
 )
+
+func init() {
+	if env, ok := os.LookupEnv("ENV"); ok {
+		if isProduction := strings.TrimSpace(env) == "production"; isProduction {
+			setupProdEnv()
+			return
+		}
+	}
+	setupDevEnv()
+}
+
+func setupDevEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func setupProdEnv() {
+	gin.SetMode(gin.ReleaseMode)
+}
 
 func main() {
 	r := gin.Default()
